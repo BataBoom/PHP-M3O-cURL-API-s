7bcf9db19a0b9914a53396e47546dd0b1c571623
C4 <56499256+BataBoom@users.noreply.github.com>
2022-03-14 18:38:58 -0600

    Create qrGenerator.php

---
+++ qrGenerator.php
@@ -0,0 +1,30 @@
+<?php
+//require cURL class, API Keys, Etc
+require 'includes/config/config.php';
+function QR($uid){
+global $qr;
+$curl = new Curl();
+$curl->setBasicAuthentication('Authorization', $qr);
+$curl->setHeader('Content-Type', 'application/json');
+$curl->post('https://api.m3o.com/v1/qr/Generate', [
+    'size' => 300,
+    'text' => "https://bataboom.bet/profile.php?uid=$uid"
+    
+]);
+if ($curl->error) {
+    echo 'Error: ' . $curl->errorCode . ': ' . $curl->errorMessage . "\n";
+} else {
+    $fetchResponse = $curl->response;
+    $jsonEncoded = json_encode($fetchResponse);
+    $fetchQR = json_decode($jsonEncoded, true);
+    $grabQR = $fetchQR['qr'];
+    $curl->close();
+    //return $grabQR;
+    $curl = new Curl();
+    $curl->setOpt(CURLOPT_FOLLOWLOCATION, true);
+    $curl->download($grabQR, "tmp/qr/qr-$uid.png");
+     //$curl->diagnose();
+    $curl->close();
+   
+    }
+}
